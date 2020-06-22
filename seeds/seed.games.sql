a79b2a1fb0ae0446d4585beba9eabe5657040289
JZPoteat <jzpoteat@gmail.com>
2020-06-22 11:16:10 -0400

    first commit

---
+++ seeds/seed.games.sql
@@ -0,0 +1,5 @@
+INSERT INTO games (title, est_time, importance, loc, notes)
+VALUES
+('Metro 2033 Redux', 20, 2, 'Epic', 'FPS'),
+('Mass Effect', 50, 3, 'Origin', 'RPG FPS'),
+('The Witcher 3', 300, 4, 'GOG', 'RPG');

0fc9814ea313ef01da591221501907c8304dbbb6
JZPoteat <jzpoteat@gmail.com>
2020-08-17 10:55:58 -0400

    search images

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const gamesRouter = require('./games/games-router');
 const usersRouter = require('./users/users-router');
 const reviewsRouter = require('./reviews/reviews-router');
 const authRouter = require('./auth/auth-router');
+const request = require('request');
 
 const app = express();
 
@@ -24,6 +25,22 @@ app.use('/api/games', gamesRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/reviews', reviewsRouter);
 app.use('/api/auth', authRouter);
+
+app.get('/api/search', (req, res) => {
+    console.log('params', req.originalUrl.split('?')[1]);
+    const params = req.originalUrl.split('?')[1];
+    const originalUrl = 'https://api.rawg.io/api/games';
+    const searchUrl = `${originalUrl}?${params}`;
+    request(
+      { url: searchUrl },
+      (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          return res.status(500).json({ type: 'error', message: 'something went wrong' });
+        }
+        res.json(JSON.parse(body));
+      }
+    )
+  });
 
 app.get('/', (req, res) => {
     res.send('Hello, boilerplate!');
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,5 +1,7 @@
 
 const app = require('../src/app');
+const supertest = require('supertest');
+const { expect } = require('chai');
 
 describe('App', () => {
     it('GET / responds with 200 containing "Hello, boilerplate!"', () => {
@@ -8,3 +10,14 @@ describe('App', () => {
             .expect(200, 'Hello, boilerplate!');
     });
 });
+
+describe('App', () => {
+    it.only('GET /api/jokes/random responds with 200 containing random joke', () => {
+        return supertest(app)
+        .get('/api/jokes/random?search=halo')
+        .then(res => {
+            console.log('here is the response', res.body);
+            expect(200);
+        })
+    })
+})
